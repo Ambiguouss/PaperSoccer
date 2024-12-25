97e2cc85396fa7ba15ad10e43587b01b070f5552
Maciej Brzozowski <maciek.brzozowski149@gmail.com>
2024-12-25 22:51:58 +0100

    delete comments

--- Players/MonteCarloTrainer.py
+++ Players/MonteCarloTrainer.py
@@ -51,16 +51,9 @@ class Node:
     def expand(self):
         if self.is_leaf():
             for v in self.valid_moves:
-            
                 #very bad
                 b = deepcopy(self.board)
                 b.make_moves(v)
-                #for e in v:
-                #    e_coord = tuple(a + b for a, b in zip(b.ball, e))
-                #    #nie updatuje touched_fields?
-                #    b.board_graph.edges[(b.ball,e_coord)]['weight']=-1
-                #    b.touched_fields.add(e_coord)
-                #    b.ball=e_coord[1]
                 self.children.append(Node(b,v,self))
 
     def selection(self):
@@ -111,27 +104,3 @@ class MonteCarloTrainer():
         return tree
 
     
-
-    #def update_board(self,board):
-    #    self.board=board
-#
-    #def make_moves(self,board):
-    #    self.update_board(board)
-    #    ball = self.board.ball
-    #    move = []
-    #    while ball in self.board.touched_fields:
-    #        valid_moves=[]
-    #        for e in self.board.board_graph.edges(ball):
-    #            if self.board.board_graph.edges[e]['weight']==0:
-    #                valid_moves.append(e)
-    #        if len(valid_moves)==0:
-    #            break
-    #        move_to_add=valid_moves[np.random.choice(len(valid_moves))]
-    #        self.board.board_graph.edges[move_to_add]['weight']=1
-    #        move_coord=tuple(b - a for a, b in zip(ball, move_to_add[1]))
-    #        move.append(move_coord)
-    #        ball = move_to_add[1]
-    #    return move
-
-
-                
